05bba9f81de78d20717968e4c52645b05d4b1aff
Mizu <mizu@blackicegames.de>
2016-09-22 13:52:28 +0200

    Pin Change interrupt on PB0 for edge detection

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#define SIGNAL_PIN (1<<PB0)
+
+ISR(PCINT0_vect){
+
+}
+
 void init(){
 	//interrupt enable
 	//enable input interrupt
+	PCICR|=(1<<PCIE0); //enable inputinterrupt for PCINT[7:0] (Port B)
+}
+
+void initTimer(){
 }
 
 void configOutput(){
+	DDRB|=SIGNAL_PIN;   //output
+	PORTB&=~SIGNAL_PIN; //zero
+	PCMSK0&=~SIGNAL_PIN; //disable pinchange interrupt ?needed
 }
 
 void configInput(){
+	DDRB&=~SIGNAL_PIN; //input
+	PORTB|=SIGNAL_PIN; //pullup
+	PCMSK0|=SIGNAL_PIN; //enable pinchange interrupt
 }
 
 void pollsignal(){
